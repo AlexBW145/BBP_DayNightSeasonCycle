c2feeff3b448db8314876af0772561070211a290
AlexBW145 <92541723+AlexBW145@users.noreply.github.com>
2024-03-30 16:16:24 -0500

    Delete obj/Release/netstandard2.0 directory

--- obj/Release/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BaldiPlus_Seasons
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "DaytimeSeasonCycle";
-        public const string PLUGIN_NAME = "DaytimeSeasonCycle";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
